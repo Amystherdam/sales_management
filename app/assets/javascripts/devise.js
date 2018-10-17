b45414796594120f8fee679df1a29d1df067666d
Amystherdam <pro.lucas07@gmail.com>
2018-10-17 16:57:06 -0300

    Alterações na tela de login. Adesão do template e mais...

---
+++ app/assets/javascripts/devise.js
@@ -0,0 +1,10 @@
+//= require vendor/jquery/jquery-3.2.1.min.js
+//= require vendor/bootstrap/js/popper.js
+//= require vendor/bootstrap/js/bootstrap.min.js
+//= require vendor/select2/select2.min.js
+//= require vendor/tilt/tilt.jquery.min.js
+//= require js.1/main.js
+
+$('.js-tilt').tilt({
+    scale: 1.1
+})
