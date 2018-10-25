afbe8e9994aff05b781a516d3c9fd23df7626ed6
Amystherdam <pro.lucas07@gmail.com>
2018-10-25 16:55:31 -0300

    Testando consultas com o banco

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,4 +12,12 @@ class HomeController < ApplicationController
 
   def detalhes
   end
+
+  def buscarData
+  end
+
+  def resultado
+    @buscar = Business.where("date_service >= ? and date_service <= ?", "2018-10-01", "2018-10-10")
+    @dados_venda = Business.all 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'index', to: 'home#index'
   get 'vendedores', to: 'home#vendedores'
   get 'detalhes', to: 'home#detalhes'
-
+  get 'buscarData', to: 'home#buscarData'
+  get 'resultado', to: 'home#resultado'
   devise_for :members
   
   root 'home#index'
