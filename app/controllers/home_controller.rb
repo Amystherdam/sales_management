f0b02774f38382c71c15b7a3b55766ada9b70630
Amystherdam <pro.lucas07@gmail.com>
2018-11-01 16:52:19 -0300

    Ainda ordenando o ranking de faturamento

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,10 +16,18 @@ class HomeController < ApplicationController
   def buscarData
   end
 
+  def seller_amount(vendedor, buscar)
+    buscar.where(seller_code: vendedor.seller_code).sum(:amount)
+  end
+
   def resultado
     @dados_venda = Business.all 
     @buscar = Business.where("date_service >= ? and date_service <= ?", params[:date_initial], params[:date_end])
     @vendedores = @buscar.select("distinct on (seller_code) *")
+    @busca_total_vendas = @buscar.sum(:amount)
+    @vendedores.each do |v|
+      puts seller_amount(v, @buscar)
+    end
   end 
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
     def seller_amount(vendedor, buscar)
         buscar.where(seller_code: vendedor.seller_code).sum(:amount)
-      end
+    end
 end
